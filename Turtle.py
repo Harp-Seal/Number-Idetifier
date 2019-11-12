bacfe5376133f936320d3377d379ff8d35250d93
Harp-Seal <56898342+Harp-Seal@users.noreply.github.com>
2019-11-12 11:28:43 -0500

    Create Turtle.py

---
+++ Turtle.py
@@ -0,0 +1,28 @@
+import turtle 
+wn = turtle.Screen()
+wn.bgcolor("blue")
+turtle.pendown
+turtle.forward(50)
+turtle.rotate(90)
+wn.bgcolor("red")
+turtle.forward(50)
+turtle.rotate(60)
+wn.bgcolor("blue")
+turtle.forward(30)
+harp == True
+x == 1
+if harp == True
+    turtle.forward(2)
+    turtle.rotate(1)
+    x == x + 1
+    if x == 90:
+        then harp == False
+turtle.penup
+for x in range(80,100):
+    turtle.forward(10)
+    wn.bgcolor("yellow")
+    turtle.pendown
+    turtle.forward(11)
+    turtle.rotate(5)
+    x == x + 1
+    turtle.penup
